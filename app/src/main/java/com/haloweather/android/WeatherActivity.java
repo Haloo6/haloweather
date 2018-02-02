c777fff76908c7bbaf08fa76dab5a3e5435ccc9d
Halo <Halo@gmail.com>
2018-02-02 15:22:21 +0800

    增加后台自动更新天气的功能且修改程序图标和名称。

--- app/src/main/java/com/haloweather/android/WeatherActivity.java
+++ app/src/main/java/com/haloweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.haloweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.haloweather.android.gson.Forecast;
 import com.haloweather.android.gson.Weather;
+import com.haloweather.android.service.AutoUpdateService;
 import com.haloweather.android.util.HttpUtil;
 import com.haloweather.android.util.Utility;
 
@@ -56,7 +58,7 @@ public class WeatherActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= 21){
             View decorView = getWindow().getDecorView(); //得到当前活动的decorView
             decorView.setSystemUiVisibility( //改变系统显示
-            View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             //这两个参数表示活动的布局会显示在状态栏上面
             getWindow().setStatusBarColor(Color.TRANSPARENT);//将状态栏设为透明色
         }
@@ -192,6 +194,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE); //将scrollView重新设为可见
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent); //激活服务
     }
 
     private void loadBingPic(){
